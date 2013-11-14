1c313098a8feff2a880b0c9a138fbac07bd42cc2
Ryan Kelly <ryan@rfk.id.au>
2013-11-14 12:39:23 +1100

    Skip some failing tests with emscripten on 64bit host

--- rpython/jit/backend/asmjs/test/test_recursive.py
+++ rpython/jit/backend/asmjs/test/test_recursive.py
@@ -1,4 +1,4 @@
-
+import os
 import py
 from rpython.jit.metainterp.test.test_recursive import RecursiveTests
 from rpython.jit.backend.asmjs.test.test_basic import JitASMJSMixin
@@ -6,4 +6,11 @@ from rpython.jit.backend.asmjs.test.test_basic import JitASMJSMixin
 class TestRecursive(JitASMJSMixin, RecursiveTests):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_recursive.py
-    pass
+
+    def test_directly_call_assembler_virtualizable_force1(self):
+        if 'x86_64' in os.uname():
+            py.test.skip("not working on x86_64 host system")
+
+    def test_directly_call_assembler_virtualizable_force_blackhole(self):
+        if 'x86_64' in os.uname():
+            py.test.skip("not working on x86_64 host system")
--- rpython/jit/backend/asmjs/test/test_virtualref.py
+++ rpython/jit/backend/asmjs/test/test_virtualref.py
@@ -1,8 +1,13 @@
-
+import os
+import py
 from rpython.jit.metainterp.test.test_virtualref import VRefTests
 from rpython.jit.backend.asmjs.test.test_basic import JitASMJSMixin
 
 class TestVRef(JitASMJSMixin, VRefTests):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_virtualref.py
-    pass
+
+    def test_alloc_virtualref_and_then_alloc_structure(self):
+        if 'x86_64' in os.uname():
+            py.test.skip("not working on x86_64 host system")
+
