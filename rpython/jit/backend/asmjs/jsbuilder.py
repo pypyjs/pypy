d3cdb6482d489145c23eac388542ae1df2677cbd
Ryan Kelly <ryan@rfk.id.au>
2013-11-19 23:33:58 +1100

    Fix float formatting for rpython

--- rpython/jit/backend/asmjs/jsbuilder.py
+++ rpython/jit/backend/asmjs/jsbuilder.py
@@ -140,7 +140,7 @@ class ASMJSBuilder(object):
             refval = (rffi.cast(lltype.Signed, val.getref_base()))
             self.emit(str(refval))
         elif isinstance(val, ConstFloat):
-            self.emit("%17g" % (val.getfloat(),))
+            self.emit("%.17g" % (val.getfloat(),))
         else:
             raise RuntimeError("Unknown js value type: %s" % (val,))
 
